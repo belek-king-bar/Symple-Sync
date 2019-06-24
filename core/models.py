8783c21b3a80165e955b5c5564f6df1c3ea411d7
Esenbekov <jagger773@gmail.com>
2019-06-24 16:17:37 +0600

    Add django-rest-framework

--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Email(models.Model):
   snippet = ArrayField(models.CharField(max_length=200))
+  userId  = models.CharField(max_length=200)
 
   def __str__(self):
     return self.snippet
