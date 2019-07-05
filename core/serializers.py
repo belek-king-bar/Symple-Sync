0a9bdbd93eb3c2caf0f66c6e2975108303e3a2d2
Esenbekov <jagger773@gmail.com>
2019-07-05 10:09:02 +0600

    Gmail authorization, retrieve & save messages complete

--- core/serializers.py
+++ core/serializers.py
@@ -1,1 +1,23 @@
 from rest_framework import serializers
+
+from core.models import Message
+
+
+class InlineMessageSerializer(object):
+    pass
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    tag_name = serializers.SerializerMethodField(read_only=True, source='tag')
+    service_name = serializers.SerializerMethodField(read_only=True, source='service')
+
+    def get_tag_name(self, message):
+        return message.tag.name
+
+    def get_service_name(self, message):
+        return message.service.name
+
+    class Meta:
+        model = Message
+        fields = ('id', 'user', 'tag', 'tag_name', 'service', 'service_name',
+                  'timestamp', 'user_name','text','created_at')
--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('doGetSlackMessages', views.ReceiveSlackListView.as_view(), name='doGetSlackMessages'),
     path('doGetSlackOauthCode', views.ReceiveSlackCodeOauthView.as_view(), name='doGetSlackOauthCode'),
-    path('doGetGmailMessages', views.RecieveEmailListView.as_view(), name='doGetGmailMessages'),
+    path('doGetGmailOauthCode', views.ReceiveGmailCodeOauthView.as_view(), name='doGetGmailOauthCode'),
+    path('doGetGmailMessages', views.RecieveGmailListView.as_view(), name='doGetGmailMessages'),
+    path('doSaveGmailMessages', views.SaveGmailListView.as_view(), name='doSaveGmailMessages'),
 ]
