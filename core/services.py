e3ac2980bd4195ecb40ff4a9d855fa56349b3c4a
Esenbekov <jagger773@gmail.com>
2019-07-05 16:30:44 +0600

    Replace getting by index with first method

--- core/services.py
+++ core/services.py
@@ -5,7 +5,7 @@ from oauth2client.client import flow_from_clientsecrets
 from django.db import transaction
 from core.exceptions import NoEmailFoundError
 from core.models import Message, Tag, Token, Service
-from project.settings import SCOPES, REDIRECT_URI, GOOGLE_OAUTH2_CLIENT_SECRETS_JSON, GMAIL_CLIENT_ID, \
+from project.settings import SCOPES, GOOGLE_REDIRECT_URI, GOOGLE_OAUTH2_CLIENT_SECRETS_JSON, GMAIL_CLIENT_ID, \
     GMAIL_CLIENT_SECRET, GOOGLE_AUTH_URL, GOOGLE_USER_AGENT
 from django.conf import settings
 import requests
@@ -28,15 +28,15 @@ class GoogleService:
     @transaction.atomic
     def receive_emails(cls, request):
         service = Service.objects.filter(name='gmail').first()
-        token = Token.objects.filter(service=service[0])
-        tags = Tag.objects.filter(service=service[0])
+        token = Token.objects.filter(service=service)
+        tags = Tag.objects.filter(service=service)
         creds = oauth2client.client.GoogleCredentials(token[0].access_token, GMAIL_CLIENT_ID, GMAIL_CLIENT_SECRET,
                                                      token[0].refresh_token, None,
                                                       GOOGLE_AUTH_URL,GOOGLE_USER_AGENT)
         http = creds.authorize(httplib2.Http())
         creds.refresh(http)
         gmail_service = build('gmail', 'v1', credentials=creds)
-        response = gmail_service.users().messages().list(userId='me', labelIds=[tags[0].name]).execute()
+        response = gmail_service.users().messages().list(userId='me', labelIds=[tags.name]).execute()
         messages = response.get('messages', [])
         if not messages:
             raise NoEmailFoundError()
@@ -45,7 +45,7 @@ class GoogleService:
                 msg = gmail_service.users().messages().get(userId='me', id=message['id']).execute()
                 headers = msg['payload']['headers']
                 delivered_to, date = retrieve_messages(headers)
-                Message.objects.create(service=service[0], tag=tags[0], text=msg['snippet'],
+                Message.objects.create(service=service, tag=tags, text=msg['snippet'],
                                        user_name=delivered_to, timestamp=date)
 
 
@@ -132,14 +132,14 @@ class OAuthAuthorization:
 
     @classmethod
     def gmail_authorization(cls, code):
-        service = Service.objects.filter(name='gmail')
-        token = Token.objects.filter(service=service[0])
+        service = Service.objects.filter(name='gmail').first()
+        token = Token.objects.filter(service=service)
         if code and not token:
             exchange_token = code
             flow = flow_from_clientsecrets(GOOGLE_OAUTH2_CLIENT_SECRETS_JSON, ' '.join(SCOPES))
-            flow.redirect_uri = REDIRECT_URI
+            flow.redirect_uri = GOOGLE_REDIRECT_URI
             credentials = flow.step2_exchange(exchange_token)
-            Token.objects.create(service=service[0], access_token=credentials.access_token,
+            Token.objects.create(service=service, access_token=credentials.access_token,
                                  refresh_token=credentials.refresh_token)
 
 
